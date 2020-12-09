503746ebf4d1ca1d91586251ad4602de63584e60
Juan Manzano <juancamilo106055@gmail.com>
2020-12-09 11:17:10 -0500

    Paquete de servicios creado

---
+++ src/main/java/com/clearminds/jcmv/servicios/ServicioBase.java
@@ -0,0 +1,26 @@
+package com.clearminds.jcmv.servicios;
+
+import java.sql.Connection;
+
+import com.clearminds.jcmv.bdd.ConexionBDD;
+
+public class ServicioBase {
+	Connection conexion = null;
+	
+	public void abrirConexion(){
+		conexion = ConexionBDD.obtenerConexion();			
+	}
+	
+	public void cerrarConexion(){
+		
+		try {
+			if (conexion!=null) {
+				conexion.close();
+				System.out.println("Conexión cerrada");							
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error al cerrar la conexión");
+		}
+	}
+}
---
+++ src/main/java/com/clearminds/jcmv/servicios/ServicioEstudiante.java
@@ -0,0 +1,5 @@
+package com.clearminds.jcmv.servicios;
+
+public class ServicioEstudiante extends ServicioBase {
+
+}
